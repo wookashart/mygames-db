2e78b1bb45607d0d8f3d183425bba172f7363986
Łukasz Krawczyk <lukasz.krawczyk@vml.com>
2021-10-05 18:42:31 +0200

    Skeleton added to datepicker - #1

--- src/components/common/form/DatePicker.tsx
+++ src/components/common/form/DatePicker.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // === Components === //
 import { DesktopDatePicker, LocalizationProvider } from '@mui/lab';
 import { TextField } from '@material-ui/core';
+import { Skeleton } from '@mui/material';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 
 interface DatePickerProps {
@@ -13,26 +14,39 @@ interface DatePickerProps {
 }
 
 const DatePicker = ({ value, label, handleChange }: DatePickerProps) => {
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(true);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DesktopDatePicker
-          label={label}
-          inputFormat="dd/MM/yyyy"
-          value={value}
-          onChange={(value) => handleChange(value)}
-          renderInput={(params) => {
-            return (
-              <TextField
-                className="customDataPicker"
-                fullWidth
-                variant="filled"
-                {...(params as any)}
-              />
-            );
-          }}
-        />
-      </LocalizationProvider>
+      {visible ? (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DesktopDatePicker
+            label={label}
+            inputFormat="dd/MM/yyyy"
+            value={value}
+            onChange={(value) => handleChange(value)}
+            renderInput={(params) => {
+              return (
+                <TextField
+                  className="customDataPicker"
+                  fullWidth
+                  variant="filled"
+                  {...(params as any)}
+                />
+              );
+            }}
+          />
+        </LocalizationProvider>
+      ) : (
+        <Skeleton variant="rectangular" height={56} />
+      )}
+
       <style jsx global>{`
         .customDataPicker button {
           color: white;
